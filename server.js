891b835f5d953344ccf201197f00dd20e14d5657
Eric Rossi <rossieric@hotmail.com>
2020-01-25 14:33:55 -0300

    Organização do projeto (forma errada)

--- server.js
+++ server.js
@@ -2,18 +2,35 @@ const http = require('http');
 
 const servidor = http.createServer(
     function(req, resp){
-resp.end(`
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1> Casa do Código </h1>
-            </body> 
-        </html>
-        `);
-    }
-);
+
+       let html = '';
+       if(req.url == '/'){
+            html = `
+                <html>
+                    <head>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1> Casa do Código </h1>
+                    </body> 
+                </html>
+                `;
+        } else if(req.url == '/livros'){
+            html = `
+                <html>
+                    <head>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1> Lista de livros </h1>
+                    </body> 
+                </html>
+                `;
+        } 
+
+    resp.end(html);
+    });
+
 servidor.listen(3000);
 
 
