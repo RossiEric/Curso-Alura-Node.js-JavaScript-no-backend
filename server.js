aac1c8ddecc37b6aec07085d3de6e98df2bb5334
Eric Rossi <rossieric@hotmail.com>
2020-01-25 16:45:23 -0300

    nodemon - Agilizando o desenvolvimento
    nodemon, --save-dev

--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 arquivo apenas apra criação do servidor
 */
 
+//get em modulo capsula para express customizado 
 const app = require('./src/config/custom-express');
 
+//abre porta 3000 para local host
 app.listen(3000, function(){
     console.log('servidor rodando http://localhost:3000/');
 });
